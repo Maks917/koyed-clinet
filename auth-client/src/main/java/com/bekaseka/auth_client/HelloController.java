05337d8ed46acc701a011841c24a33b0e841ee25
Maks917 <66785055+Maks917@users.noreply.github.com>
2024-08-10 08:40:25 +0500

    Add files via upload

---
+++ auth-client/src/main/java/com/bekaseka/auth_client/HelloController.java
@@ -0,0 +1,14 @@
+package com.bekaseka.auth_client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    @ResponseBody
+    String hello(){
+        return "hello World";
+    }
+}
